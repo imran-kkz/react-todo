084fb8bc6c767dab7f72a6ffc758b71c6a50fea2
imran-kkz <imranrali1@gmail.com>
2020-10-19 22:55:55 -0400

    understanding components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-function MyInfo() {
-  return(
-  <div>
-  <h1>Imran</h1>
-  <p>I'm learning React!</p>
-  <ul>
-    <li>India</li>
-    <li>Miami</li>
-    <li>New York</li>
-  </ul>
-  </div>
-  )
-}
+import MyInfo from './components/MyInfo'
 
 ReactDOM.render(
   <MyInfo/>, document.getElementById("root")
