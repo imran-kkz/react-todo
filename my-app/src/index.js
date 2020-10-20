fb7ab8c88510850ab648b122a15f3741855ff49b
imran-kkz <imranrali1@gmail.com>
2020-10-19 23:03:32 -0400

    adding boilerplate

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MyInfo from './components/MyInfo'
 
 ReactDOM.render(
-  <MyInfo/>, document.getElementById("root")
+  <h1>Hi</h1>,
+  document.getElementById("root")
 )
