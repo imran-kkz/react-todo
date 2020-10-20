420e60d8d7439d540be62d884f6274156300a178
imran-kkz <imranrali1@gmail.com>
2020-10-19 23:21:47 -0400

    understanding parent-child relationships

---
+++ my-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import MainContent from './MainContent'
+import Navbar from './Navbar'
+
+function App() {
+    return(
+        <div>
+        <Navbar/>
+        <MainContent/>
+        <Footer/>
+        </div>
+    )
+}
+
+export default App
---
+++ my-app/src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContent() {
+    return(
+        <div>
+            <h2>this is my main content</h2>
+        </div>
+    )
+}
+
+export default MainContent
