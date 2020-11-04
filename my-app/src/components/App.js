015b444c3c4274734a58490ad99364ef2582a0f0
Imran Ali <imranrali1@gmail.com>
2020-11-03 22:18:39 -0500

    edit

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ToDoItem from './ToDoItem'
 import todoData from './todoData'
 import "./styles.css"
 
-function App (){
-
-    const todoList = todoData.map(item=> <ToDoItem key={item.id} text={item.text} check={item.completed}/>)
+class App extends Component {
+constructor() {
+    super()
+    this.state = todoData.map(item=> <ToDoItem key={item.id} text={item.text} check={item.completed}/>)
+}
+render(){
+    // const todoList = todoData.map(item=> <ToDoItem key={item.id} text={item.text} check={item.completed}/>)
     return(
         <div className="todo-list">
-            {todoList}
+            {this.state}
+            {/* {todoList} */}
         </div>
-    )
+        )
+    }
 }
 export default App
