d48fcb56b2be15dc7765f75be2a11131c1334b8e
Imran Ali <imranrali1@gmail.com>
2020-11-05 23:29:26 -0500

    changing console log output

--- my-app/src/components/ToDoItem.js
+++ my-app/src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ToDoItem (props){
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.check}/>
+            <input type="checkbox" checked={props.item.check} onChange={() => console.log("button changed!")}/>
             <p>{props.item.text}</p>
         </div>
     )
