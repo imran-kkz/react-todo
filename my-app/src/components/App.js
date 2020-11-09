e900bcc156233c69e3f9e6a484baaf893a9723a6
Imran Ali <imranrali1@gmail.com>
2020-11-09 18:43:15 -0500

    final todo

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -9,9 +9,27 @@ constructor() {
     this.state = {
         todos: todoData
     }
+    this.handleClick = this.handleClick.bind(this)
 }
+
+handleClick(id) {
+    console.log('test' + id)
+    this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+            if (todo.id === id) {
+                todo.check = !todo.check
+            }
+            return todo
+        })
+        return {
+            todos: updatedTodos
+        }
+    })
+}
+
 render(){
-    const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} />)
+    const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} handleClick={this.handleClick} />)
+
     return(
         <div className="todo-list">
             {todoItems}
--- my-app/src/components/ToDoItem.js
+++ my-app/src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ToDoItem (props){
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.check} onChange={() => console.log("button changed!")}/>
+            <input type="checkbox" checked={props.item.check} onChange={() => props.handleClick(props.item.id)}/>
             <p>{props.item.text}</p>
         </div>
     )
